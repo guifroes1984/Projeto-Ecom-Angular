fb6aef7d6d33f2aa8430cc538c5eb0901f0b13c9
guifroes1984 <guilhermefroes26@gmail.com>
2023-11-15 02:18:23 -0300

    Start na API. Configuracao do Material. Criando os components: admin, cliente, inscrever e login.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { InscreverComponent } from './inscrever/inscrever.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'inscrever', component: InscreverComponent},
+  { path: 'cliente', loadChildren: () => import('./cliente/cliente.module').then(m => m.ClienteModule) },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
