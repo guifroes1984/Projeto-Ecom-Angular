0d923de7ee549348260c88cbf0f47960ec2a3310
guifroes1984 <guilhermefroes26@gmail.com>
2023-11-20 23:28:07 -0300

    Criando a pagina de inscricao, chamando a API e servico de autenticacao.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,22 +10,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemoAngularMaterialModule } from './DemoAngularMaterialModule';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { MatIconModule } from "@angular/material/icon";
 
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    SignupComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    DemoAngularMaterialModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        SignupComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        DemoAngularMaterialModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        MatIconModule
+    ]
 })
 export class AppModule { }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +10,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+
+  signupForm!: FormGroup;
+  hidePassword = true;
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar, 
+    private authService: AuthService,
+    private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      name: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]], 
+      confirmPassword: [null, [Validators.required]],
+    })
+  }
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  onSubmit(): void {
+    const password = this.signupForm.get('password')?.value;
+    const confirmPassword = this.signupForm.get('confirmPassword')?.value;
+
+    if (password !== confirmPassword) {
+      this.snackBar.open('Senhas não conferem.', 'Fechar', {duration: 5000, panelClass: 'error-snackbar'});
+      return;
+    }
+
+    this.authService.register(this.signupForm.value).subscribe(
+      (response) => {
+        this.snackBar.open('Cadastro bem sucedido!', 'Fechar', {duration: 5000});
+        this.router.navigateByUrl("/login");
+      },
+      (error) => {
+        this.snackBar.open('Falha na inscrição. Por favor, tente novamente.', 'Fechar', {duration: 5000, panelClass: 'error-snackbar'});
+      }
+    )
+  }
 
 }
