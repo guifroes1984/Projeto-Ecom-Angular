c5fcfdd1f1c52d83848d241eaf632b0b6cba7e64
guifroes1984 <guilhermefroes26@gmail.com>
2023-11-22 22:27:55 -0300

    Criando pagina de login, chamada de API e servico de armazenamento.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  loginForm!: FormGroup;
+
+  hidePassword = true;
+
+  constructor( 
+    private formBuilder: FormBuilder, 
+    private authService: AuthService, 
+    private snackBar: MatSnackBar, 
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [null, [Validators.required]], 
+      password: [null, [Validators.required]],
+    })
+  }
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  onSubmit(): void {
+    const username = this.loginForm.get('email')!.value;
+    const password = this.loginForm.get('password')!.value;
+
+    this.authService.login(username, password).subscribe(
+      (res) => {
+        this.snackBar.open('Sucesso no login', 'OK', { duration: 5000});
+      }, 
+      (error) => {
+        this.snackBar.open('Credenciais incorretas', 'ERROR', { duration: 5000});
+      }
+    )
+  }
 
 }
