fb11e9248d2c23cbddc07700593da68f0bfb80b5
guifroes1984 <guilhermefroes26@gmail.com>
2023-11-24 15:38:26 -0300

    Postar pagina de categorias.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DemoAngularMaterialModule } from '../DemoAngularMaterialModule';
+import { PostCategoryComponent } from './components/post-category/post-category.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    DashboardComponent
+    DashboardComponent,
+    PostCategoryComponent
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    DemoAngularMaterialModule
   ]
 })
 export class AdminModule { }
