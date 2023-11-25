5f0324deacfbf6ade56fc2db15fa790826e8b8c9
guifroes1984 <guilhermefroes26@gmail.com>
2023-11-25 02:03:22 -0300

    Publicar pagina do produto, visualizacao de imagem e selecao de categoria.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,13 +8,15 @@ import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { DemoAngularMaterialModule } from '../DemoAngularMaterialModule';
 import { PostCategoryComponent } from './components/post-category/post-category.component';
+import { PostProductComponent } from './components/post-product/post-product.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
     DashboardComponent,
-    PostCategoryComponent
+    PostCategoryComponent,
+    PostProductComponent
   ],
   imports: [
     CommonModule,
