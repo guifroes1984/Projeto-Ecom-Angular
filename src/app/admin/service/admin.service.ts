1ae654cfd8823df94133da8d2da9f030e5daf388
guifroes1984 <guilhermefroes26@gmail.com>
2023-11-27 20:47:09 -0300

    Mostrando produtos ao administrador.

--- src/app/admin/service/admin.service.ts
+++ src/app/admin/service/admin.service.ts
@@ -30,6 +30,12 @@ export class AdminService {
     })
   }
 
+  getAllProducts(): Observable<any> {
+    return this.http.get(BASIC_URL + 'api/admin/products', {
+      headers: this.createAuthorizationHeader(),
+    })
+  }
+
   private createAuthorizationHeader(): HttpHeaders {
     return new HttpHeaders().set(
       'Authorization', 'Bearer ' + UserStorageService.getToken()
